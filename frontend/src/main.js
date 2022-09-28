afcb970472531657ef65addad6f6f3818f901e68
MatejZat <mato.zatkuliak@gmail.com>
2022-09-28 16:16:28 +0200

    Initial commit

---
+++ frontend/src/main.js
@@ -0,0 +1,21 @@
+//VUE
+import { createApp } from 'vue'
+
+// APP.VUE
+import App from './App.vue'
+
+
+// SERVICE WORKER
+import './registerServiceWorker'
+
+
+// ROUTER
+import router from './router'
+
+
+// STORE
+import store from './store'
+
+
+// CREATE APP
+createApp(App).use(store).use(router).mount('#app')
